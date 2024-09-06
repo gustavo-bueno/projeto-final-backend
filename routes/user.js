0d2aab211e7e10d9d88963199979207c5395b53c
gustavo-bueno <gustavobcarvalho22@gmail.com>
2024-09-06 18:25:28 -0300

    feat: adjust user routes

--- routes/user.js
+++ routes/user.js
@@ -6,11 +6,11 @@ const UserDAO = require('../services/UserDAO');
 const { isAdmin } = require('../middlewares/auth');
 
 router.get("/", async (req, res) => {
-  const { limit, page } = req.query;
+  const { limit = 10, page = 1 } = req.query;
 
   try {
     const users = await UserDAO.list(page, limit)
-    res.status(201).json({ status: true, users });
+    res.status(200).json({ status: true, users });
   } catch (err) {
     res.status(500).json({ error: 'Falha ao listar usuários' });
   }
@@ -44,7 +44,6 @@ router.post("/auth", async (req, res) => {
     const token = jwt.sign({ id: user.id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
     res.json({ token });
   } catch (err) {
-    console.log(err)
     res.status(500).json({ error: 'Falha ao logar usuário' });
   }
 })
@@ -62,7 +61,6 @@ router.post("/admin", isAdmin, async (req, res) => {
     });
     res.status(201).json({ message: 'Usuário admin criado com sucesso!', admin });
   } catch (err) {
-    console.log(err)
     res.status(500).json({ error: 'Falha ao criar usuário admin' });
   }
 })
@@ -86,30 +84,30 @@ router.delete("/:id", isAdmin, async (req, res) => {
   }
 });
 
+
 router.put("/:id", async (req, res) => {
   const { id } = req.params;
   const { password, name } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
+  const hashedPassword = password ? await bcrypt.hash(password, 10) : null;
 
   try {
     const user = await UserDAO.getById(id);
     if (!user) {
-      return res.status(404).json({ error: 'User não encontrado' });
+      return res.status(404).json({ error: 'Usuario não encontrado' });
     }
-    if (req.user.id != id && req.user.role !== 'admin') {
-      return res.status(403).json({ error: 'Não é possível editar informações dos usuários admin' });
+    if (req.user.id !== id && req.user.role !== 'admin') {
+      return res.status(403).json({ error: 'Não é possível editar informações de outro usuário' });
     }
 
-    if(name) {
+    if (name) {
       user.name = name;
     }
 
-    if(password) {
+    if (hashedPassword) {
       user.password = hashedPassword;
     }
 
     await user.save();
-
     res.json({ message: 'Usuário atualizado com sucesso!' });
   } catch (err) {
     res.status(500).json({ error: 'Erro ao atualizar as informações do usuário.' });
